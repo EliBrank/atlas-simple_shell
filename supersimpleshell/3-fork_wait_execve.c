0fe3381b25d9861fc2060e4cad47d0067a7d6cc8
EliBrank <eliasbrinkman@gmail.com>
2024-03-21 22:03:08 +0000

    update fork wait execve func

--- supersimpleshell/3-fork_wait_execve.c
+++ supersimpleshell/3-fork_wait_execve.c
@@ -9,23 +9,16 @@
 
 int main(void)
 {
-	pid_t my_pid;
 	pid_t child_pid;
-	printf("Before fork\n");
+	int i;
+	int numProc;
 
-	child_pid = fork();
-	if (child_pid == -1)
+	numProc = 5;
+
+	for (i = 0; i < numProc; i++)
 	{
-		perror("Error:");
-		return (1);
-	}
-	my_pid = getpid();
-	printf("My pid is %u\n", my_pid);
 
-	if (child_pid == 0)
-		printf("(%u) Noooooooooo!\n", my_pid);
-	else
-		printf("(%u) %u, I am your father \n", my_pid, child_pid);
+	}
 
 	return (0);
 }

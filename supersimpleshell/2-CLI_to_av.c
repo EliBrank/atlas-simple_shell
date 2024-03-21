0eb0422bda24ac21c9ddce617e1071ca10ed0f38
EliBrank <eliasbrinkman@gmail.com>
2024-03-21 19:13:16 +0000

    update CLI to argv func

--- supersimpleshell/2-CLI_to_av.c
+++ supersimpleshell/2-CLI_to_av.c
@@ -5,26 +5,64 @@
 
 int main(void)
 {
-	int i;
-	char *array;
+	int i, j;
+	int count = 0;
+	char **array;
 	char *buffer;
-	size_t bufsize = 4096;
+	size_t bufsize = 4095;
 	char *portion;
-	char *delimiter = " ";
+	char *delim = " \n";
+	ssize_t bytes;
 
 	buffer = (char *)malloc(sizeof(char) * bufsize);
 	if (buffer == NULL)
 		return (-1);
 
-	while (1)
+	bytes = getline(&buffer, &bufsize, stdin);
+	if (bytes == -1)
 	{
-		printf("$ ");
-		getline(&buffer, &bufsize, stdin);
-		printf("%s", buffer);
-		if (strncmp(buffer, "exit", 4) == 0)
-			break;
+		free(buffer);
+		return (-1);
 	}
 
+	for (i = 0; i < strlen(buffer); i++)
+	{
+		if (buffer[i] == ' ')
+			count++;
+	}
+	if (buffer[strlen(buffer) - 1] != ' ')
+		count++;
+
+
+	array = (char **)malloc(sizeof(char *) * (count + 1));
+	if (array == NULL)
+	{
+		free(buffer);
+		return (-1);
+	}
+
+	portion = strtok(buffer, delim);
+	i = 0;
+	while (portion != NULL)
+	{
+		array[i] = strdup(portion);
+		if (array[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+				free(array[j]);
+			free(array);
+			free(buffer);
+			return (-1);
+		}
+		printf("%s\n", array[i]);
+		portion = strtok(NULL, delim);
+		i++;
+	}
+	array[i] = NULL;
+
+	for (i = 0; i < count; i++)
+		free(array[i]);
+	free(array);
 	free(buffer);
 
 	return (0);

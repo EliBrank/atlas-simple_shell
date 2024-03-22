7ec923e2f92b2210361604b56c3c61542da427ac
EliBrank <eliasbrinkman@gmail.com>
2024-03-22 19:49:44 +0000

    start adding comments to SSSh func

--- supersimpleshell/4-shell.c
+++ supersimpleshell/4-shell.c
@@ -10,12 +10,29 @@
 int main(void)
 {
 	char *buffer;
-	size_t bufsize = 4096;
+	size_t bufsize = 4095;
+	int i, j, count;
+	char **array;
+	char *buffer, *portion;
+	ssize_t bytes;
+	char *delim = " \n";
 
+	/* init count, allocate mem for buffer */
+	count = 0;
 	buffer = (char *)malloc(sizeof(char) * bufsize);
 	if (buffer == NULL)
 		return (-1);
 
+	/* counts args (strings) in buffer */
+	for (i = 0; i < strlen(buffer); i++)
+	{
+		if (buffer[i] == ' ')
+			count++;
+	}
+	if (buffer[strlen(buffer) - 1] != ' ')
+		count++;
+
+	/* central loop to get user input */
 	while (1)
 	{
 		printf("$ ");
@@ -24,14 +41,6 @@ int main(void)
 		if (strncmp(buffer, "exit", 4) == 0)
 			break;
 	}
-
-	for (i = 0; i < strlen(buffer); i++)
-	{
-		if (buffer[i] == ' ')
-			count++;
-	}
-	if (buffer[strlen(buffer) - 1] != ' ')
-		count++;
 
 
 

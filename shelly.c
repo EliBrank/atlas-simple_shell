0d85be366cf7a4e857338b002b5ca293672899f1
EliBrank <eliasbrinkman@gmail.com>
2024-03-31 19:37:55 +0000

    rework env_get to work on string array instead of linked list

--- shelly.c
+++ shelly.c
@@ -7,13 +7,10 @@
  */
 int main(int argc, char **argv, char **envp)
 {
-	char **user_args, **path_value_array;
+	char **user_args;
 	ssize_t bytes;
 	char *buffer, *path_value_full, *new_arg_one;
 	size_t bufsize = 4096;
-
-	/* set up environment */
-	char *env = env_setup(envp);
 
 	/* argc and argv aren't necessary, so cast as void */
 	(void)argc;
